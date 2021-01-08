108241b9fe9246a7a52b91b1a07afcc573465279
NahinMontoya <73256114+NahinMontoya@users.noreply.github.com>
2021-01-07 21:41:07 -0500

    se modifica la clase materia

--- TalllerRefactoring/src/modelos/Materia.java
+++ TalllerRefactoring/src/modelos/Materia.java
@@ -8,4 +8,50 @@ public class Materia {
     public double notaFinal;
     public double notaTotal;
     
+public Materia(String codigo, String nombre, String facultad) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.facultad = facultad;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getFacultad() {
+		return facultad;
+	}
+	public void setFacultad(String facultad) {
+		this.facultad = facultad;
+	}
+	public double getNotaInicial() {
+		return notaInicial;
+	}
+	public void setNotaInicial(double notaInicial) {
+		this.notaInicial = notaInicial;
+	}
+	public double getNotaFinal() {
+		return notaFinal;
+	}
+	public void setNotaFinal(double notaFinal) {
+		this.notaFinal = notaFinal;
+	}
+	public double getNotaTotal() {
+		return notaTotal;
+	}
+	public void setNotaTotal(double notaTotal) {
+		this.notaTotal = notaTotal;
+	}
+    
+  
+
 }
